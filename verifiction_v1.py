452573179f88aa709f037147f5aedc25e308b0a9
Devdsby12 <144847905+Devdsby12@users.noreply.github.com>
2026-05-27 17:54:23 +0530

    Create verifiction_v1.py

---
+++ verifiction_v1.py
@@ -0,0 +1,13 @@
+user1 = {"email":"xt3@gmail.com","password":"LOl99a" , "data": input()}
+user2 = {"email":"xt@gmail.com","password":"LOl99" , "data": input()}
+
+email = "xt@gmail.com"
+Pass = "LO99"
+username = user2
+if email == username["email"] and Pass == username["password"]:
+    print (username["data"])
+else :
+    print("invalid details")
+
+
+# basic idea how verification works 
